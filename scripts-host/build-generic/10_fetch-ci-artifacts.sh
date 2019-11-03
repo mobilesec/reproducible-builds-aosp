251960813e64fa135db309c911568e6f495ca218
Manuel Pöll <manuel.poell.mail@gmail.com>
2019-11-03 01:16:34 +0100

    Download all artifacts for Android CI build

--- scripts-host/build-generic/10_fetch-ci-artifacts.sh
+++ scripts-host/build-generic/10_fetch-ci-artifacts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -x
 # Argument sanity check
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <BUILD_NUMBER> <BUILD_TARGET>"
@@ -16,21 +17,45 @@ if [ -z "${RB_AOSP_BASE+x}" ]; then
 	mkdir -p "${RB_AOSP_BASE}"
 fi
 
-function fetchFromGoogleCI {
+function fetchFromAndroidCI {
     FILE="$1"
 
     # The actual file content does not have a public link, only a Artifact viewer link is available. Retrieve raw file via some simple web scrapping
     # Actual file link is stored in JS object. Extract JSON literal from JS source via sed, then extract property via jq
     curl "https://ci.android.com/builds/submitted/${BUILD_NUMBER}/${BUILD_TARGET}/latest/view/${FILE}" -L \
         | grep "artifacts/${FILE}" \
-        | sed -n "s/^[^{]*\([^}]*\}\).*$/\1/p" \
+        | sed -E -e "s/^[ \t]+var[ \t]+JSVariables[ \t=]+//" -e "s/[ \t]*;[ \t]*$//" \
         | jq -r '."artifactUrl"' \
         > "${IMAGE_DIR}/${FILE}.link"
     curl "$(cat "${IMAGE_DIR}/${FILE}.link")" -L > "${IMAGE_DIR}/${FILE}" # Fetch actual ${FILE}
 }
 
+function fetchArtifactList {
+   	curl "https://ci.android.com/builds/submitted/${BUILD_NUMBER}/${BUILD_TARGET}/latest" -L \
+	| grep -P 'var[ ]+JSVariables[ =]+\{.*}[ ]*;' \
+	| sed -E -e "s/^[ \t]+var[ \t]+JSVariables[ \t=]+//" -e "s/[ \t]*;[ \t]*$//" \
+	| jq -r '."artifacts"[]."name"' \
+	> "${IMAGE_DIR}/artifacts_list"
+}
+
+
 # Fetch manifest from Google CI build
 BUILD_ENV="GoogleCI"
 IMAGE_DIR="${RB_AOSP_BASE}/build/${BUILD_NUMBER}/${BUILD_TARGET}/${BUILD_ENV}"
 mkdir -p "${IMAGE_DIR}"
-fetchFromGoogleCI "manifest_${BUILD_NUMBER}.xml"
+
+# Create artifact list
+fetchArtifactList
+
+# Iterate all artifacts and download them
+ARTIFACTS=($(cat "${IMAGE_DIR}/artifacts_list"))
+for ARTIFACT in "${ARTIFACTS[@]}"; do
+	echo ${ARTIFACT}
+	if [[ "$ARTIFACT" == *"/"* ]]; then
+		DIR="${ARTIFACT%%/*}"
+		mkdir -p "${IMAGE_DIR}/${DIR}"
+	fi
+
+	fetchFromAndroidCI ${ARTIFACT}
+done
+
