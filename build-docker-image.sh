5410c92cbba9e32aa03fa0faa8671bafac94c770
Manuel Pöll <manuel.poell.mail@gmail.com>
2019-11-13 23:58:57 +0100

    [docker] move mounts to run

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -4,7 +4,4 @@ docker build \
     --target builder \
     --tag "mpoell/rb-aosp:latest" \
     --no-cache=true \
-    --mount "type=bind,source=${HOME}/aosp/src/.repo/project-objects,target=/root/aosp/src/.repo/project-objects" \
-    --mount "type=bind,source=${HOME}/aosp/src/.repo/project.list,target=/root/aosp/src/.repo/project.list" \
-    --mount "type=bind,source=${HOME}/aosp/src/.repo/projects,target=/root/aosp/src/.repo/projects" \
     .
---
+++ run-docker-image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run \
+    --name "rb-aosp_android-10.0.0_r11_crosshatch" \
+    --mount "type=bind,source=${HOME}/aosp/src/.repo/project-objects,target=/root/aosp/src/.repo/project-objects" \
+    --mount "type=bind,source=${HOME}/aosp/src/.repo/project.list,target=/root/aosp/src/.repo/project.list" \
+    --mount "type=bind,source=${HOME}/aosp/src/.repo/projects,target=/root/aosp/src/.repo/projects" \
+    "mpoell/rb-aosp:latest"
