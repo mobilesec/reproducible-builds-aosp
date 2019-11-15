5b5ed477ab017efb657f2c9bd8a273672650d310
Manuel Pöll <manuel.poell.mail@gmail.com>
2019-11-15 20:48:27 +0100

    [docker] Compose run commands via heredoc

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN apt-get update \
     && sh "./scripts/shared/setup/04_config-git.sh" \
     && rm -rf "/var/lib/apt/lists/*"
 
-CMD /bin/bash -c ". \"./scripts/docker/setup-runtime/01_set-runtime-path.sh\"; bash \"./scripts/shared/build-device/10_clone-src-device.sh\" \"android-10.0.0_r11\"; bash \"./scripts/shared/build-device/11_fetch-extract-vendor.sh\" \"QP1A.191105.004\" \"crosshatch\"; bash \"./scripts/shared/build-device/12_build-device.sh\" \"android-10.0.0_r11\" \"aosp_crosshatch-user\" \"crosshatch\"; bash \"./scripts/shared/build-device/13_fetch-extract-factory-images.sh\" \"android-10.0.0_r11\" \"QP1A.191105.004\" \"crosshatch\"; bash \"./scripts/docker/analysis/21_diffoscope-files.sh\" \"/root/aosp/build/android-10.0.0_r11/crosshatch-user/Google\" \"/root/aosp/build/android-10.0.0_r11/aosp_crosshatch-user/Debian10\" \"/root/aosp/diff/android-10.0.0_r11_crosshatch-user_Google__android-10.0.0_r11_aosp_crosshatch-user_Debian10\"; bash"
+CMD bash
--- run-docker-image.sh
+++ run-docker-image.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
+AOSP_REF="android-10.0.0_r11"
+BUILD_ID="QP1A.191105.004"
+DEVICE_CODENAME="crosshatch"
+GOOGLE_BUILD_TARGET="${DEVICE_CODENAME}-user"
+GOOGLE_BUILD_ENV="Google"
+RB_AOSP_BASE="/root/aosp"
+RB_BUILD_TARGET="aosp_${DEVICE_CODENAME}-user"
+RB_BUILD_ENV="Debian10"
+
+compose_cmds() {
+cat <<EOF | tr '\n' '; '
+source \"./scripts/docker/setup-runtime/01_set-runtime-path.sh\"
+bash \"./scripts/shared/build-device/10_clone-src-device.sh\" \"${AOSP_REF}\"
+bash \"./scripts/shared/build-device/11_fetch-extract-vendor.sh\" \"${BUILD_ID}\" \"${DEVICE_CODENAME}\"
+bash \"./scripts/shared/build-device/12_build-device.sh\" \"${AOSP_REF}\" \"${RB_BUILD_TARGET}\" \"${DEVICE_CODENAME}\"
+bash \"./scripts/shared/build-device/13_fetch-extract-factory-images.sh\" \"${AOSP_REF}\" \"${BUILD_ID}\" \"${DEVICE_CODENAME}\"
+bash \"./scripts/docker/analysis/21_diffoscope-files.sh\" \
+    \"${RB_AOSP_BASE}/build/${AOSP_REF}/${GOOGLE_BUILD_TARGET}/${GOOGLE_BUILD_ENV}\" \
+    \"${RB_AOSP_BASE}/build/${AOSP_REF}/${RB_BUILD_TARGET}/${RB_BUILD_ENV}\" \
+    \"${RB_AOSP_BASE}/diff/${AOSP_REF}_${GOOGLE_BUILD_TARGET}_${GOOGLE_BUILD_ENV}__${AOSP_REF}_${RB_BUILD_TARGET}_${RB_BUILD_ENV}\"
+bash
+EOF
+}
+
+DOCKER_RUN_CMD="/bin\/bash -c \"$(compose_cmds)\""
+
 docker run \
-    --name "rb-aosp_android-10.0.0_r11_crosshatch" \
+    --name "${AOSP_REF}_${GOOGLE_BUILD_TARGET}_${GOOGLE_BUILD_ENV}__${AOSP_REF}_${RB_BUILD_TARGET}_${RB_BUILD_ENV}" \
     --mount "type=bind,source=${HOME}/aosp/src/.repo/project-objects,target=/root/aosp/src/.repo/project-objects" \
     --mount "type=bind,source=${HOME}/aosp/src/.repo/project.list,target=/root/aosp/src/.repo/project.list" \
     --mount "type=bind,source=${HOME}/aosp/src/.repo/projects,target=/root/aosp/src/.repo/projects" \
     --mount "type=bind,source=${HOME}/aosp/diff,target=/root/aosp/diff" \
-    "mpoell/rb-aosp:latest"
+    "mpoell/rb-aosp:latest" "${DOCKER_RUN_CMD}"
