4485112b2571a9a83fe59b4269f2f9f482f780fa
Manuel Pöll <manuel.poell.mail@gmail.com>
2019-11-15 19:54:22 +0100

    [docker] further improvements

--- run-docker-image.sh
+++ run-docker-image.sh
@@ -5,4 +5,5 @@ docker run \
     --mount "type=bind,source=${HOME}/aosp/src/.repo/project-objects,target=/root/aosp/src/.repo/project-objects" \
     --mount "type=bind,source=${HOME}/aosp/src/.repo/project.list,target=/root/aosp/src/.repo/project.list" \
     --mount "type=bind,source=${HOME}/aosp/src/.repo/projects,target=/root/aosp/src/.repo/projects" \
+    --mount "type=bind,source=${HOME}/aosp/diff,target=/root/aosp/diff" \
     "mpoell/rb-aosp:latest"
