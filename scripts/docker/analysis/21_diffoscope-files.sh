04f23d0360f9f11372059cf78e372dca5cdf035b
Manuel Pöll <manuel.poell.mail@gmail.com>
2019-11-26 23:25:52 +0100

    Rework diffoscope mount wrapper* Ensure ext images are mounted ahead of diffoscope call (regardless if sparse or not)

--- scripts/docker/analysis/21_diffoscope-files.sh
+++ scripts/docker/analysis/21_diffoscope-files.sh
@@ -18,41 +18,47 @@ if [[ -z "${RB_AOSP_BASE+x}" ]]; then
 	mkdir -p "${RB_AOSP_BASE}"
 fi
 
-function mountSparseImage {
+function decompressSparseImage {
     IMG_SPARSE="$1"
-    IMG_RAW="${IMG_SPARSE}.raw"
+    IMG_RAW="$2"
 
     # Deomcpress into raw ext2/3/4 partition image
     "${SIMG_2_IMG_BIN}" "${IMG_SPARSE}" "${IMG_RAW}"
-    # To avoid accidental changes, mount ro (also simg2img has a bug where Android 10 images can't be mounted rw)
-    # This bug is also the reason we can't give ${IMG_RAW} directly to diffoscope (it attempts and fails a rw mount)
-    mkdir -p "${IMG_RAW}_mount/"
-    mount -o ro "${IMG_RAW}" "${IMG_RAW}_mount/"
 }
 
-function unmountSparseImage {
-    IMG_SPARSE="$1"
-    IMG_RAW="${IMG_SPARSE}.raw"
+function mountExtImage {
+    IMG_EXT="$1"
+    IMG_MOUNT="$2"
 
-    umount "${IMG_RAW}_mount/"
-    rmdir "${IMG_RAW}_mount/"
-    rm "${IMG_RAW}"
+    # To avoid accidental changes + unsupported feature flag, mount ro
+    mkdir -p "${IMG_MOUNT}"
+    mount -o ro "${IMG_EXT}" "${IMG_MOUNT}"
+}
+
+function unmountExtImage {
+    IMG_EXT="$1"
+    IMG_MOUNT="$2"
+
+    umount "${IMG_MOUNT}"
+    rmdir "${IMG_MOUNT}"
 }
 
 function diffoscopeFile {
+    # Original input paramts
     IN_1="$1"
     IN_2="$2"
     DIFF_OUT="$3"
-
+    # Mutable diffoscope params
+    DIFF_IN_1="${IN_1}"
+    DIFF_IN_2="${IN_2}"
+    
     # Detect sparse images
-    IN_1_SPARSE_IMG=false
-    IN_2_SPARSE_IMG=false
     set +e # Disable early exit
-    file "${IN_1}" | grep 'Android sparse image'
+    file "${DIFF_IN_1}" | grep 'Android sparse image'
     if [[ "$?" -eq 0 ]]; then
         IN_1_SPARSE_IMG=true
     fi
-    file "${IN_2}" | grep 'Android sparse image'
+    file "${DIFF_IN_2}" | grep 'Android sparse image'
     if [[ "$?" -eq 0 ]]; then
         IN_2_SPARSE_IMG=true
     fi
@@ -60,31 +66,69 @@ function diffoscopeFile {
 
     # Convert them to raw images that can be readily mounted
     if [[ "${IN_1_SPARSE_IMG}" = true ]]; then
-        mountSparseImage "${IN_1}"
-        DIFF_IN_1="${IN_1}.raw_mount"
-    else
-        DIFF_IN_1="${IN_1}"
+        decompressSparseImage "${DIFF_IN_1}" "${DIFF_IN_1}.raw"
+        DIFF_IN_1="${DIFF_IN_1}.raw"
     fi
     if [[ "${IN_2_SPARSE_IMG}" = true ]]; then
-        mountSparseImage "${IN_2}"
-        DIFF_IN_2="${IN_2}.raw_mount"
-    else
-        DIFF_IN_2="${IN_2}"
+        decompressSparseImage "${DIFF_IN_2}" "${DIFF_IN_2}.raw"
+        DIFF_IN_2="${DIFF_IN_2}.raw"
     fi
 
+    # Detect ext2/3/4 images. Usually these can be handled by diffoscope just fine, however...
+    # Starting with Android 10 all ext4 images use some special dedup/compression feature which results in
+    # `EXT4-fs (loop3): couldn't mount RDWR because of unsupported optional features (4000)`
+    # when attempting a standard rw mount. Thus mount these images ro before calling diffoscope with the mount point
+    set +e # Disable early exit
+    file "${DIFF_IN_1}" | grep -P '(ext2)|(ext3)|(ext4)'
+    if [[ "$?" -eq 0 ]]; then
+        IN_1_EXT_IMG=true
+    fi
+    file "${DIFF_IN_2}" | grep -P '(ext2)|(ext3)|(ext4)'
+    if [[ "$?" -eq 0 ]]; then
+        IN_2_EXT_IMG=true
+    fi
+    set -e # Re-enable early exit
+
+    # Convert them to raw images that can be readily mounted
+    if [[ "${IN_1_EXT_IMG}" = true ]]; then
+        mountExtImage "${DIFF_IN_1}" "${DIFF_IN_1}_mount"
+        DIFF_IN_1="${DIFF_IN_1}_mount"
+    fi
+    if [[ "${IN_2_EXT_IMG}" = true ]]; then
+        mountExtImage "${DIFF_IN_2}" "${DIFF_IN_2}_mount"
+        DIFF_IN_2="${DIFF_IN_2}_mount"
+    fi
+
+    set +e # Disable early exit
     diffoscope --output-empty --progress \
             --exclude-directory-metadata=recursive --exclude 'com.android.runtime.release.apex' \
             --text "${DIFF_OUT}.txt" \
             --html-dir "${DIFF_OUT}.html-dir" \
-            "${DIFF_IN_1}" \
-            "${DIFF_IN_2}"
+            "${DIFF_IN_1}" "${DIFF_IN_2}"
+    set -e # Re-enable early exit
 
-    # Unmount sparse images if applicable
+    # Unmount ext images if applicable
+    if [[ "${IN_1_EXT_IMG}" = true ]]; then
+        if [[ "${IN_1_SPARSE_IMG}" = true ]]; then
+            unmountExtImage "${IN_1}.raw" "${DIFF_IN_1}"
+        else
+            unmountExtImage "${IN_1}" "${DIFF_IN_1}"
+        fi
+    fi
+    if [[ "${IN_2_EXT_IMG}" = true ]]; then
+        if [[ "${IN_2_SPARSE_IMG}" = true ]]; then
+            unmountExtImage "${IN_2}.raw" "${DIFF_IN_2}"
+        else
+            unmountExtImage "${IN_2}" "${DIFF_IN_2}"
+        fi
+    fi
+
+    # Delete sparse images if applicable
     if [[ "${IN_1_SPARSE_IMG}" = true ]]; then
-        unmountSparseImage "${IN_1}"
+        rm "${IN_1}.raw"
     fi
     if [[ "${IN_2_SPARSE_IMG}" = true ]]; then
-        unmountSparseImage "${IN_2}"
+        rm "${IN_2}.raw"
     fi
 }
 
