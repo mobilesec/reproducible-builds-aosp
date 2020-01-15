01b09769d04e1174e9ea289cd3c51385bd03cc79
Manuel Pöll <manuel.poell.mail@gmail.com>
2020-01-15 20:51:32 +0100

    Proper heredoc for .profile

--- scripts/shared/setup/05_config_profile.sh
+++ scripts/shared/setup/05_config_profile.sh
@@ -1,14 +1,14 @@
 
 # Setup the .profile file to include common local bin paths during startup
-cat >> "$HOME/.profile" <<EOF
+cat >> "$HOME/.test" << 'EOF'
 
 # set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/bin" ] ; then
-    PATH="$HOME/bin:$PATH"
+if [ -d "${HOME}/bin" ] ; then
+    PATH="${HOME}/bin:${PATH}"
 fi
 
 # set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/.local/bin" ] ; then
-    PATH="$HOME/.local/bin:$PATH"
+if [ -d "${HOME}/.local/bin" ] ; then
+    PATH="${HOME}/.local/bin:${PATH}"
 fi
 EOF
