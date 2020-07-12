48b6b71898b788b94a4c81628bcba68025703cd2
Manuel Pöll <manuel.poell.mail@gmail.com>
2020-07-12 16:16:23 +0200

    Fix lpunpack invocationt to be generic and applicable for both flows

--- scripts/shared/analysis/19_lpunpack-super-imgs.sh
+++ scripts/shared/analysis/19_lpunpack-super-imgs.sh
@@ -2,8 +2,9 @@
 set -o errexit -o nounset -o pipefail -o xtrace
 
 unpackSuper() {
-    local -r BUILD_ENV="$1"
-    local -r TARGET_DIR="${RB_AOSP_BASE}/build/${BUILD_NUMBER}/${BUILD_TARGET}/${BUILD_ENV}"
+    local -r BUILD_TARGET="$1"
+    local -r BUILD_ENV="$2"
+    local -r TARGET_DIR="${RB_AOSP_BASE}/build/${AOSP_REF_OR_BUILD_NUMBER}/${BUILD_TARGET}/${BUILD_ENV}"
 
     # Only perform unpacking if either system or vendor are missing and we do have super image
     if [[ ! -f "${TARGET_DIR}/system.img" ]] || [[ ! -f "${TARGET_DIR}/vendor.img" ]]; then
@@ -15,14 +16,16 @@ unpackSuper() {
 
 main() {
     # Argument sanity check
-    if [[ "$#" -ne 2 ]]; then
-        echo "Usage: $0 <BUILD_NUMBER> <BUILD_TARGET>"
-        echo "BUILD_NUMBER: Google internal incremental build number that identifies each build, see https://android.googlesource.com/platform/build/+/master/Changes.md#BUILD_NUMBER"
-        echo "BUILD_TARGET: Build target as choosen in lunch (consist of <TARGET_PRODUCT>-<TARGET_BUILD_VARIANT>"
+    if [[ "$#" -ne 3 ]]; then
+        echo "Usage: $0 [ <AOSP_REF> | <BUILD_NUMBER> ] <GOOGLE_BUILD_TARGET> <RB_BUILD_TARGET>"
+        echo "AOSP_REF or BUILD_NUMBER: Tag/Branch reference or Google internal build number"
+        echo "GOOGLE_BUILD_TARGET: Google build target as choosen in lunch (consist of <TARGET_PRODUCT>-<TARGET_BUILD_VARIANT>"
+        echo "RB_BUILD_TARGET: Our build target as choosen in lunch (consist of <TARGET_PRODUCT>-<TARGET_BUILD_VARIANT>"
         exit 1
     fi
-    local -r BUILD_NUMBER="$1"
-    local -r BUILD_TARGET="$2"
+    local -r AOSP_REF_OR_BUILD_NUMBER="$1"
+    local -r GOOGLE_BUILD_TARGET="$2"
+    local -r RB_BUILD_TARGET="$3"
     # Reproducible base directory
     if [[ -z "${RB_AOSP_BASE+x}" ]]; then
         # Use default location
@@ -36,8 +39,8 @@ main() {
     local -r GOOGLE_BUILD_ENV="Google"
     local -r RB_BUILD_ENV="$(lsb_release -si)$(lsb_release -sr)"
     
-    unpackSuper "${GOOGLE_BUILD_ENV}"
-    unpackSuper "${RB_BUILD_ENV}"
+    unpackSuper "${GOOGLE_BUILD_TARGET}" "${GOOGLE_BUILD_ENV}"
+    unpackSuper "${RB_BUILD_TARGET}" "${RB_BUILD_ENV}"
 }
 
 main "$@"
