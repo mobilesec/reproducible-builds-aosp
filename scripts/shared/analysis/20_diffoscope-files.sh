6c72105971c8410299c7578ec6097bb40d98f0d1
Manuel Pöll <manuel.poell.mail@gmail.com>
2020-07-14 23:25:46 +0200

    Fix hardcoded value in guestmount call

--- scripts/shared/analysis/20_diffoscope-files.sh
+++ scripts/shared/analysis/20_diffoscope-files.sh
@@ -29,7 +29,7 @@ function preProcessImage {
         # guestfs allows working with complex file systems images (e.g. LVM volume groups with multiple LVs) and thus requires
         # us to be explicit about what part of image we want to mount. Running `virt-filesystems -a system.img.raw` returns the
         # name of the "virtual" device and can be directly fed to guestmount (usually this is /dev/sda)
-        guestmount -a "${DIFF_IN_RESOLVED}" -m "$(virt-filesystems -a system.img.raw)" --ro "${DIFF_IN_RESOLVED}.mount"
+        guestmount -a "${DIFF_IN_RESOLVED}" -m "$(virt-filesystems -a "${DIFF_IN_RESOLVED}")" --ro "${DIFF_IN_RESOLVED}.mount"
         sudo bindfs -u "${USER}" -g "${USER}" "--create-for-user=${USER}" "--create-for-group=${USER}" "${DIFF_IN_RESOLVED}.mount" "${DIFF_IN_RESOLVED}.bind"
         eval $DIFF_IN_META="${DIFF_IN_RESOLVED}.bind"
 
